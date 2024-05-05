3993d1e6293f61f215ec351ef0101b80862db40c
Aleksey25 <aleksey25@csltd.com.ua>
2024-05-05 21:50:29 +0300

    Proj

--- src/main/java/org/example/db/CollectionOperations.java
+++ src/main/java/org/example/db/CollectionOperations.java
@@ -14,8 +14,6 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 
 import static org.example.utils.Utils.*;
-import static org.example.utils.Utils.logOperationResult;
-
 
 public class CollectionOperations{
 
